59e84afd27454395f8cda53daff654d455815316
publicthreat <publicthreat@proton.me>
2024-10-31 04:11:44 -0500

    moved installation processes to installer

--- start.sh
+++ start.sh
@@ -5,35 +5,17 @@ VENV_DIR=venv/bin/activate
 DL_DIR=${PWD}/dl_dir
 MUSIC_DIR=~/Music
 
-# Python Management
-# - make venv if it doesn't exist
-if [[ ! -d venv ]]; then
-	echo "Making venv"
-	python3 -m venv venv
-fi
-
-# - activate venv
+# Activate Venv
 source venv/bin/activate
 
-# - (quiet) install requirements
-if [[ -f requirements.txt ]]; then
-	pip install -q -r requirements.txt
-else
-	echo "No requirements.txt found"
-	exit 1
-fi
-
-# DL_DIR Management
-if [[ ! -d $DL_DIR ]]; then
-	mkdir $DL_DIR
-fi
-
-cd $DL_DIR
-
-# Downloading Songs
+# Get URL from user
 echo "Enter URL:"
 read USER_URL
 
+# Moving to download directory
+cd $DL_DIR
+
+# Action from URL
 if [[ $USER_URL == *"spotify"* ]]; then
 	echo "Spotify URL detected"
 	spotdl $USER_URL
@@ -41,10 +23,10 @@ elif [[ $USER_URL == *"youtube"* ]]; then
 	echo "YouTube URL detected"
 	yt-dlp -x --audio-format mp3 $USER_URL
 elif [[ $USER_URL == "" ]]; then
-	echo "No download URL given"
+	echo "No download URL given; Sorting files"
 fi
 
-# - back out to main project directory
+# Back out to main project directory
 cd ..
 
 # Sort files in DL_DIR
