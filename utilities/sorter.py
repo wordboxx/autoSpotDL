f02d2a934dfdc6110cda2905ec7ef8792e763897
kingdavid <honeybaptism@proton.me>
2025-02-18 17:35:22 -0600

    added comments

--- utilities/sorter.py
+++ utilities/sorter.py
@@ -5,9 +5,11 @@ import shutil
 from mutagen.easyid3 import EasyID3
 
 def sort():
+    # Filepaths injected from terminal by "start.sh".
     DL_DIR = sys.argv[1]
     MUSIC_DIR = sys.argv[2]
 
+    # Loop through all files in the download directory, "DL_DIR".
     for file in os.listdir(DL_DIR):
         print(f"Now sorting: {file}")
 
@@ -21,21 +23,22 @@ def sort():
         artist_dir = MUSIC_DIR + "/" + file_artist
         album_dir = artist_dir + "/" + file_album
 
-        # - If there aren't artist and album directories, create them.
+        # If there aren't artist and album directories, create them.
         if not os.path.exists(artist_dir):
             os.makedirs(artist_dir)
         if not os.path.exists(album_dir):
             os.makedirs(album_dir)
 
-        # - if the file already exists, skip.
+        # If the file already exists,
+        # delete newest file and skip.
         if os.path.exists(album_dir + "/" + file):
             print(f"{file} already exists at location {album_dir}; deleting...")
             os.remove(file)
             continue
         else:
-            # - move file to artist/album directory.
+            # Move file to artist/album directory.
             shutil.move(DL_DIR + "/" + file, album_dir)
 
-# MAIN
+# If program is called, rather than functions.
 if __name__ == "__main__":
     sort()
