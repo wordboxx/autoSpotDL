4fe26eb0fb422948f84a4b1414f78445e240d3fd
kingdavid <honeybaptism@proton.me>
2025-02-18 18:04:48 -0600

    sourced utility shell files

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Directories
+# Directories.
 PROJECT_DIR=${PWD}
 VENV_DIR=$PROJECT_DIR/venv
 UTILITIES_DIR=$PROJECT_DIR/utilities
@@ -9,25 +9,14 @@ MUSIC_DIR=~/Music
 
 # Install a Python Venv if there isn't one yet.
 if [ ! -d venv ]; then
-    $UTILITIES_DIR/./installer.sh
+    . $UTILITIES_DIR/./installer.sh
 fi
 
-# Activate Venv
-source venv/bin/activate
+# Activate Venv.
+source $VENV_DIR/bin/activate
 
-# Get URL from user
-echo "Enter URL:"
-read -r USER_URL
-
-# Moving to download directory (or exit if directory non-existent)
-cd "$DL_DIR" || exit
-
-# Download files from URL into DL_DIR
-if [[ $USER_URL == *"spotify"* ]]; then
-    spotdl "$USER_URL"
-elif [[ $USER_URL == *"youtu"* ]]; then
-    yt-dlp -x --audio-format mp3 "$USER_URL"
-fi
+# Download songs.
+. $UTILITIES_DIR/./downloader.sh
 
 # Back out to main project directory.
 cd ..
